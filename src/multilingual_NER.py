8082386d0616465ef10ff09b3b5c2d85fb6f8658
Tamara Atanasoska <atanasoska.tamara@gmail.com>
2023-03-27 22:07:05 +0200

    Fix another pair of wrong brackets

--- src/multilingual_NER.py
+++ src/multilingual_NER.py
@@ -21,6 +21,6 @@ def recognise_named_entities_multilingual(df):
     This function takes a dataframe and finds the named entities for the two
     relevant columns.
     """
-    df["id1_text_multi_NER"] = process_data[df["id1_text"].tolist()]
-    df["id2_text_multi_NER"] = process_data[df["id2_text"].tolist()]
+    df["id1_text_multi_NER"] = process_data(df["id1_text"].tolist())
+    df["id2_text_multi_NER"] = process_data(df["id2_text"].tolist())
     return df
